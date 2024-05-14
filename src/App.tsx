6bd9d380e5d9d5a448d457180198281da358cdbb
litae <109706689+qkdflrgs@users.noreply.github.com>
2024-05-14 21:26:31 +0900

    feat(fetch): set Data fetching (#3)
    * fix: remove unused files and code
    
    * feat(fetch): add FullScreenMessage component
    
    * feat(fetch): add Data Fetching to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,48 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
 import styles from './App.module.scss'
 import classNames from 'classnames/bind'
+import { useEffect, useState } from 'react'
+import FullScreenMessage from './components/shared/FullScreenMessage'
 
 const cx = classNames.bind(styles)
 
 function App() {
-  return (
-    <div className={cx('container')}>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+  const [wedding, setWedding] = useState(null)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [isError, setIsError] = useState<boolean>(false)
+
+  // 데이터 호출
+  useEffect(() => {
+    setIsLoading(true)
+    fetch('http://localhost:8888/wedding')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('데이터를 불러오지 못했습니다')
+        }
+
+        return response.json()
+      })
+      .then((data) => {
+        setWedding(data)
+        setIsLoading(false)
+      })
+      .catch((e) => {
+        console.log(e)
+        setIsError(true)
+      })
+      .finally(() => {
+        setIsLoading(false)
+      })
+  }, [])
+
+  if (isLoading) {
+    return <FullScreenMessage type="loading" />
+  }
+
+  if (isError) {
+    return <FullScreenMessage type="error" />
+  }
+
+  return <div className={cx('container')}>{JSON.stringify(wedding)}</div>
 }
 
 export default App
