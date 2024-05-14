1f14c7bf63c04224952b66efd51f9c34d38cafa9
qkdflrgs <qkdflrgs@gmail.com>
2024-05-14 21:25:23 +0900

    feat(fetch): add Data Fetching to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,48 @@
 import styles from './App.module.scss'
 import classNames from 'classnames/bind'
+import { useEffect, useState } from 'react'
+import FullScreenMessage from './components/shared/FullScreenMessage'
 
 const cx = classNames.bind(styles)
 
 function App() {
-  return <div className={cx('container')}>App</div>
+  const [wedding, setWedding] = useState(null)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [isError, setIsError] = useState<boolean>(false)
+
+  // 데이터 호출
+  useEffect(() => {
+    setIsLoading(true)
+    fetch('http://localhost:8888/wedding')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('데이터를 불러오지 못했습니다')
+        }
+
+        return response.json()
+      })
+      .then((data) => {
+        setWedding(data)
+        setIsLoading(false)
+      })
+      .catch((e) => {
+        console.log(e)
+        setIsError(true)
+      })
+      .finally(() => {
+        setIsLoading(false)
+      })
+  }, [])
+
+  if (isLoading) {
+    return <FullScreenMessage type="loading" />
+  }
+
+  if (isError) {
+    return <FullScreenMessage type="error" />
+  }
+
+  return <div className={cx('container')}>{JSON.stringify(wedding)}</div>
 }
 
 export default App
