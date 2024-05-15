538602f6cab1a55b7e3fc34eebcd143122c55516
qkdflrgs <qkdflrgs@gmail.com>
2024-05-15 20:42:22 +0900

    feat(message): add TextMessage component

---
+++ src/components/shared/TextMessage.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+interface TextMessageProps {
+  children: string
+}
+
+function TextMessage({ children }: TextMessageProps) {
+  const message = children.split('\n').map((string, index, array) => {
+    return (
+      <React.Fragment>
+        {string}
+        {index === array.length - 1 ? null : <br />}
+      </React.Fragment>
+    )
+  })
+
+  return <div>{message}</div>
+}
+
+export default TextMessage
