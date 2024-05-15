56d8f4ebef6c97e513d8963824220f0bb32c6466
qkdflrgs <qkdflrgs@gmail.com>
2024-05-15 19:19:35 +0900

    feat(gallery): add ImageViewer to ImagesGallery component

--- src/components/sections/ImagesGallery.tsx
+++ src/components/sections/ImagesGallery.tsx
@@ -1,6 +1,8 @@
 import classNames from 'classnames/bind'
 import Section from '@shared/Section'
 import styles from './ImagesGallery.module.scss'
+import ImageViewer from '../ImageViewer'
+import { useState } from 'react'
 
 const cx = classNames.bind(styles)
 
@@ -9,16 +11,42 @@ interface ImagesGalleryProps {
 }
 
 function ImagesGallery({ images }: ImagesGalleryProps) {
+  const [selectedIdx, setSelectedIdx] = useState<number>(-1)
+
+  const open = selectedIdx !== -1
+
+  const handleSelectedImage = (idx: number) => {
+    setSelectedIdx(idx)
+  }
+
+  const handleClose = () => {
+    setSelectedIdx(-1)
+  }
+
   return (
-    <Section title="사진첩">
-      <ul className={cx('wrap-images')}>
-        {images.map((src, index) => (
-          <li key={index} className={cx('wrap-image')}>
-            <img src={src} alt="사진첩 이미지" />
-          </li>
-        ))}
-      </ul>
-    </Section>
+    <>
+      <Section title="사진첩">
+        <ul className={cx('wrap-images')}>
+          {images.map((src, index) => (
+            <li
+              key={index}
+              className={cx('wrap-image')}
+              onClick={() => {
+                handleSelectedImage(index)
+              }}
+            >
+              <img src={src} alt="사진첩 이미지" />
+            </li>
+          ))}
+        </ul>
+      </Section>
+      <ImageViewer
+        images={images}
+        open={open}
+        selectedIdx={selectedIdx}
+        onClose={handleClose}
+      />
+    </>
   )
 }
 
