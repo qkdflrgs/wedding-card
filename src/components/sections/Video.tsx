8344111493db954f7f45bc7a17061dadda747ac1
qkdflrgs <qkdflrgs@gmail.com>
2024-05-15 17:39:04 +0900

    feat(video): add Video component

---
+++ src/components/sections/Video.tsx
@@ -0,0 +1,22 @@
+import Section from '@shared/Section'
+import classNames from 'classnames/bind'
+import styles from './Video.module.scss'
+
+const cx = classNames.bind(styles)
+
+function Video() {
+  return (
+    <Section classNames={cx('container')}>
+      <video
+        autoPlay={true}
+        muted={true}
+        loop={true}
+        poster="/assets/poster.jpg"
+      >
+        <source src="/assets/main.mp4" type="video/mp4" />
+      </video>
+    </Section>
+  )
+}
+
+export default Video
