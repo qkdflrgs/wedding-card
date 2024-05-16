53d53d0cf7b3d0f17c922b9f546ece9b52d52e3c
litae <109706689+qkdflrgs@users.noreply.github.com>
2024-05-16 19:29:02 +0900

    feat(attend): add Attend Count Modal (#11)
    * feat(attend): add Modal component
    
    * feat(attend): add Dimmed component
    
    * feat(attend): refactoring ImageViewer component(apply Dimmed component)
    
    * feat(attend): add variable scss for z-index
    
    * feat(attend): apply Dimmed component to ImageViewer
    
    * feat(attend): add AttendCountModal component
    
    * feat(attend): add ModalContext
    
    * feat(attend): apply Modal context to App
    
    * feat(attend): add AttendCountModal to App
    
    * feat(attend): add root-portal div to html

--- src/components/ImageViewer/index.tsx
+++ src/components/ImageViewer/index.tsx
@@ -3,6 +3,7 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 import styles from './ImageViewer.module.scss'
 import 'swiper/css'
 import './swiper.css'
+import Dimmed from '@shared/Dimmed'
 
 const cx = classNames.bind(styles)
 
@@ -22,7 +23,7 @@ function ImageViewer({
   if (!open) return null
 
   return (
-    <div className={cx('dimmed')}>
+    <Dimmed>
       <CloseButton className={cx('icon-close')} onClose={onClose} />
       <Swiper
         spaceBetween={20}
@@ -38,7 +39,7 @@ function ImageViewer({
           )
         })}
       </Swiper>
-    </div>
+    </Dimmed>
   )
 }
 
