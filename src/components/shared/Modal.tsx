4552ab24a99afa0724a86043310c066ec3f84b11
qkdflrgs <qkdflrgs@gmail.com>
2024-05-16 18:29:55 +0900

    feat(attend): add Modal component

---
+++ src/components/shared/Modal.tsx
@@ -0,0 +1,46 @@
+import classNames from 'classnames/bind'
+import Dimmed from './Dimmed'
+import styles from './Modal.module.scss'
+
+const cx = classNames.bind(styles)
+
+interface ModalProps {
+  open: boolean
+  title?: string
+  body: React.ReactNode
+  leftButtonLabel?: string
+  rightButtonLabel?: string
+  onLeftButtonClick: () => void
+  onRightButtonClick: () => void
+}
+
+function Modal({
+  open,
+  title,
+  body,
+  leftButtonLabel = '닫기',
+  rightButtonLabel = '확인',
+  onLeftButtonClick,
+  onRightButtonClick,
+}: ModalProps) {
+  if (!open) return null
+
+  return (
+    <Dimmed>
+      <div className={cx('wrap-modal')}>
+        <div className={cx('wrap-body')}>
+          <div className={cx('wrap-content')}>
+            {title && <div className={cx('text-title')}>{title}</div>}
+            {body}
+          </div>
+          <div className={cx('wrap-buttons')}>
+            <button onClick={onLeftButtonClick}>{leftButtonLabel}</button>
+            <button onClick={onRightButtonClick}>{rightButtonLabel}</button>
+          </div>
+        </div>
+      </div>
+    </Dimmed>
+  )
+}
+
+export default Modal
