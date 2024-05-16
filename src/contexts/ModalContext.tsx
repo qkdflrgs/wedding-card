e9d2ba5aeabfaf8249f6812a6f807636cc4dfe98
qkdflrgs <qkdflrgs@gmail.com>
2024-05-16 19:26:12 +0900

    feat(attend): add ModalContext

---
+++ src/contexts/ModalContext.tsx
@@ -0,0 +1,56 @@
+import Modal from '@/components/shared/Modal'
+import { ComponentProps, createContext, useContext, useState } from 'react'
+import { createPortal } from 'react-dom'
+
+type ModalProps = ComponentProps<typeof Modal>
+type ModalOptions = Omit<ModalProps, 'open'>
+
+interface ModalContextValue {
+  open: (options: ModalOptions) => void
+  close: () => void
+}
+
+const Context = createContext<ModalContextValue | undefined>(undefined)
+
+const defaultValue: ModalProps = {
+  open: false,
+  body: null,
+  onLeftButtonClick: () => {},
+  onRightButtonClick: () => {},
+}
+
+export function ModalContext({ children }: { children: React.ReactNode }) {
+  const [modalState, setModalState] = useState<ModalProps>(defaultValue)
+  const $portal_root = document.getElementById('root-portal')
+  const open = (options: ModalOptions) => {
+    setModalState({
+      ...options,
+      open: true,
+    })
+  }
+  const close = () => {
+    setModalState(defaultValue)
+  }
+
+  const values = {
+    open,
+    close,
+  }
+
+  return (
+    <Context.Provider value={values}>
+      {children}
+      {$portal_root && createPortal(<Modal {...modalState} />, $portal_root)}
+    </Context.Provider>
+  )
+}
+
+export function useModalContext() {
+  const values = useContext(Context)
+
+  if (values == null) {
+    throw new Error('ModalContext 안에서 사용해주세요')
+  }
+
+  return values
+}
