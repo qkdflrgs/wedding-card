05c5c5059d1c0db33dad6d0279dc14c3b17f58af
qkdflrgs <qkdflrgs@gmail.com>
2024-05-17 14:28:42 +0900

    opti(rerender): add useCallback, memo to ModalContext

--- src/contexts/ModalContext.tsx
+++ src/contexts/ModalContext.tsx
@@ -1,5 +1,12 @@
 import Modal from '@/components/shared/Modal'
-import { ComponentProps, createContext, useContext, useState } from 'react'
+import {
+  ComponentProps,
+  createContext,
+  useCallback,
+  useContext,
+  useMemo,
+  useState,
+} from 'react'
 import { createPortal } from 'react-dom'
 
 type ModalProps = ComponentProps<typeof Modal>
@@ -22,20 +29,25 @@ const defaultValue: ModalProps = {
 export function ModalContext({ children }: { children: React.ReactNode }) {
   const [modalState, setModalState] = useState<ModalProps>(defaultValue)
   const $portal_root = document.getElementById('root-portal')
-  const open = (options: ModalOptions) => {
+
+  const open = useCallback((options: ModalOptions) => {
     setModalState({
       ...options,
       open: true,
     })
-  }
-  const close = () => {
-    setModalState(defaultValue)
-  }
+  }, [])
 
-  const values = {
-    open,
-    close,
-  }
+  const close = useCallback(() => {
+    setModalState(defaultValue)
+  }, [])
+
+  const values = useMemo(
+    () => ({
+      open,
+      close,
+    }),
+    [open, close],
+  )
 
   return (
     <Context.Provider value={values}>
